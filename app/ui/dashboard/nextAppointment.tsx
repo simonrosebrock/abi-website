9361a023b1970d74596d55daa666848ddbb0da1c
MrGameSucht <117409993+MrGameSucht@users.noreply.github.com>
2024-08-23 15:23:32 +0200

    useEffect bug fixed
    ausversehen useEffect conditionally verwendet

--- app/ui/dashboard/nextAppointment.tsx
+++ app/ui/dashboard/nextAppointment.tsx
@@ -19,44 +19,28 @@ type AppointmentProps = {
 };
 
 export function NextAppointment({termin}: QueryResultRow) {
-
+    var date = new Date("2024-08-31T22:00:00.000Z");
     if (termin != null) {
-        useEffect(() => {
-            var intervalId = setInterval(() => {
-                var countDownDate = new Date(date).getTime();
-                var now = new Date().getTime();
-                var distance = countDownDate - now;
-    
-                setDays(Math.floor(distance / (1000 * 60 * 60 * 24)));
-                setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-                setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-                setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
-                
-                if (distance < 0) {
-                    clearInterval(intervalId);
-                }
-            }, 1000)
-        }, [])
-    } else {
-        return
+        var title = termin.title;
+        var ort = termin.ort;
+        date = termin.date;
+        var start_time = termin.start_time;
+        var end_time = termin.end_time;
+        var persons = termin.helfer;
     }
-    
 
-    var title = termin.title;
-    var ort = termin.ort;
-    var date = termin.date;
-    var start_time = termin.start_time;
-    var end_time = termin.end_time;
-    var persons = termin.helfer;
+
     const [days, setDays] = useState<number>()
     const [hours, setHours] = useState<number>()
     const [minutes, setMinutes] = useState<number>()
     const [seconds, setSeconds] = useState<number>()
 
-    start_time = start_time.slice(0, -3);
-    end_time = end_time.slice(0, -3);
-
-    var gruppen = Object.keys(persons)
+    if (termin != null) {
+        start_time = start_time.slice(0, -3);
+        end_time = end_time.slice(0, -3);
+    
+        var gruppen = Object.keys(persons)
+    }
 
     useEffect(() => {
         var intervalId = setInterval(() => {
@@ -75,6 +59,9 @@ export function NextAppointment({termin}: QueryResultRow) {
         }, 1000)
     }, [])
 
+    if (termin == null) {
+        return;
+    }
     return(
         <div className="w-[250px] h-[250px] bg-white shadow-sm rounded-lg p-2 flex flex-col">
                 <h3 className="text-[#05004E] text-xl">Nächster Termin</h3>
