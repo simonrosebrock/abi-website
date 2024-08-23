3bc9726176478ef3e758aeb6b1191d08701b1738
MrGameSucht <117409993+MrGameSucht@users.noreply.github.com>
2024-08-23 15:38:22 +0200

    Fixed nextAppointment

--- app/ui/dashboard/nextAppointment.tsx
+++ app/ui/dashboard/nextAppointment.tsx
@@ -20,13 +20,14 @@ type AppointmentProps = {
 
 export function NextAppointment({termin}: QueryResultRow) {
     var date = new Date("2024-08-31T22:00:00.000Z");
+    var persons: Personen = {};
     if (termin != null) {
         var title = termin.title;
         var ort = termin.ort;
         date = termin.date;
         var start_time = termin.start_time;
         var end_time = termin.end_time;
-        var persons = termin.helfer;
+        persons = termin.helfer;
     }
 
 
@@ -34,12 +35,13 @@ export function NextAppointment({termin}: QueryResultRow) {
     const [hours, setHours] = useState<number>()
     const [minutes, setMinutes] = useState<number>()
     const [seconds, setSeconds] = useState<number>()
-
+    
+    var gruppen: String[] = [];
     if (termin != null) {
         start_time = start_time.slice(0, -3);
         end_time = end_time.slice(0, -3);
     
-        var gruppen = Object.keys(persons)
+        gruppen = Object.keys(persons)
     }
 
     useEffect(() => {
@@ -60,58 +62,60 @@ export function NextAppointment({termin}: QueryResultRow) {
     }, [])
 
     if (termin == null) {
-        return;
-    }
-    return(
-        <div className="w-[250px] h-[250px] bg-white shadow-sm rounded-lg p-2 flex flex-col">
-                <h3 className="text-[#05004E] text-xl">Nächster Termin</h3>
-                
-                <div className='h-[206px] flex'>
-                    <div className='flex flex-col'>
-                        <h4 className="text-abi-gray text-md">{title}</h4>
-                        <span className="text-abi-gray text-md">{ort}</span>
-                        <div className='mt-auto mb-auto'>
-                            {
-                                gruppen.map((gruppe, index) => (
-                                    <div key={`${gruppe}-${index}`} className="w-[165px] text-abi-gray text-sm overflow-hidden text-ellipsis whitespace-nowrap">
-                                        <span className="text-red-500">•</span>
-                                        <span>{` ${persons[gruppe].join(", ")}`}</span>
-                                    </div>
-                                ))
-                            }
+        return(<></>);
+    } else {
+        return(
+            <div className="w-[250px] h-[250px] bg-white shadow-sm rounded-lg p-2 flex flex-col">
+                    <h3 className="text-[#05004E] text-xl">Nächster Termin</h3>
+                    
+                    <div className='h-[206px] flex'>
+                        <div className='flex flex-col'>
+                            <h4 className="text-abi-gray text-md">{title}</h4>
+                            <span className="text-abi-gray text-md">{ort}</span>
+                            <div className='mt-auto mb-auto'>
+                                {
+                                    gruppen.map((gruppe, index) => (
+                                        <div key={`${gruppe}-${index}`} className="w-[165px] text-abi-gray text-sm overflow-hidden text-ellipsis whitespace-nowrap">
+                                            <span className="text-red-500">•</span>
+                                            <span>{` ${persons.gruppe.join(", ")}`}</span>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                            
+                            <span className="text-abi-gray text-md">{getDateString(date)}</span>
+                            <span className="text-abi-gray text-md">{`${start_time} - ${end_time} Uhr`}</span>
                         </div>
                         
-                        <span className="text-abi-gray text-md">{getDateString(date)}</span>
-                        <span className="text-abi-gray text-md">{`${start_time} - ${end_time} Uhr`}</span>
-                    </div>
-                    
-                    <div className="grid grid-flow-row gap-5 text-center auto-cols-max scale-50 w-[70px] origin-top-right float-right z-0 ml-auto">
-                        <div className="flex flex-col p-2 bg-[#dcfce7] rounded-box text-abi-black">
-                            <span className="countdown font-mono text-5xl">
-                            <span style={{"--value":days} as CSSProperties}></span>
-                            </span>
-                            days
-                        </div> 
-                        <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
-                            <span className="countdown font-mono text-5xl">
-                            <span style={{"--value":hours} as CSSProperties}></span>
-                            </span>
-                            hours
-                        </div> 
-                        <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
-                            <span className="countdown font-mono text-5xl">
-                            <span style={{"--value":minutes} as CSSProperties}></span>
-                            </span>
-                            min
-                        </div> 
-                        <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
-                            <span className="countdown font-mono text-5xl">
-                            <span style={{"--value":seconds} as CSSProperties}></span>
-                            </span>
-                            sec
+                        <div className="grid grid-flow-row gap-5 text-center auto-cols-max scale-50 w-[70px] origin-top-right float-right z-0 ml-auto">
+                            <div className="flex flex-col p-2 bg-[#dcfce7] rounded-box text-abi-black">
+                                <span className="countdown font-mono text-5xl">
+                                <span style={{"--value":days} as CSSProperties}></span>
+                                </span>
+                                days
+                            </div> 
+                            <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
+                                <span className="countdown font-mono text-5xl">
+                                <span style={{"--value":hours} as CSSProperties}></span>
+                                </span>
+                                hours
+                            </div> 
+                            <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
+                                <span className="countdown font-mono text-5xl">
+                                <span style={{"--value":minutes} as CSSProperties}></span>
+                                </span>
+                                min
+                            </div> 
+                            <div className="flex flex-col p-2 bg-[#DCFCE7] rounded-box text-abi-black">
+                                <span className="countdown font-mono text-5xl">
+                                <span style={{"--value":seconds} as CSSProperties}></span>
+                                </span>
+                                sec
+                            </div>
                         </div>
-                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
