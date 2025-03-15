51553c4b8ad392a46b637e3b1d1c93d7fe66541d
MrGameSucht <117409993+MrGameSucht@users.noreply.github.com>
2025-03-15 19:50:52 +0100

    Foto api rework and bigger images

--- app/passwordchange/inputField.tsx
+++ app/passwordchange/inputField.tsx
@@ -3,27 +3,43 @@ import Link from 'next/link';
 import { updateAccountPassword } from '@/app/lib/dbConnection';
 import { useRouter } from 'next/navigation';
 import { getCleanUser } from '../lib/miniFuncs';
+import { useState } from 'react';
 
 export function InputField({token, user}: {token: string, user: string}) {
+    const [info, setInfo] = useState<string>("");
     const router = useRouter();
     const cleanUser = getCleanUser(user);
 
     return(
         <form action={async (formData: FormData) => {
             const password = (formData.get('password') as string).trim();
-            if (password === "" || password == null || password.length > 255) {
-                return
+            const passwordConfirmed = (formData.get('password-confirmed') as string).trim();
+            if (password === "" || password == null || passwordConfirmed === "" || passwordConfirmed == null) {
+                setInfo("Bitte fülle alle Felder aus!");
+                return;
+            }
+            if (password.length > 255 || passwordConfirmed.length > 255) {
+                setInfo("Das Passwort ist zu lang!");
+                return;
+            }
+            if (password !== passwordConfirmed) {
+                setInfo("Die Passwörter stimmen nicht überein!");
+                return;
             }
             updateAccountPassword(token as string, password as string);
             router.push("/dashboard");
         }}> 
-            <div className='sm:mt-10'>
+            <div className='flex flex-col items-center gap-5 p-5 xs:p-10'>
                 <div className='flex justify-center text-2xl text-gray-400 gap-2'>
                     <span>{`Account:`}</span>
                     <span className='text-gray-600'>{cleanUser}</span>
                 </div>
-                <label className="input input-bordered flex items-center m-10">
-                    <input type="text" name='password' placeholder="New Password" className="grow"/>
+                <span className='text-center text-red-500'>{info}</span>
+                <label className="input input-bordered flex items-center w-full">
+                    <input type="text" name='password' placeholder="Neues Passwort" className="grow"/>
+                </label>
+                <label className="input input-bordered flex items-center w-full">
+                    <input type="text" name='password-confirmed' placeholder="Passwort bestätigen" className="grow"/>
                 </label>
             </div>
             
