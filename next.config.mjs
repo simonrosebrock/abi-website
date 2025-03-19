0c23b572c0eec3a7c9305d5fc1f3a26370d0b7e8
MrGameSucht <117409993+MrGameSucht@users.noreply.github.com>
2025-03-19 20:25:09 +0100

    proxy url finish up

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: [process.env.PROXY_URL, "unhqdt5ayffgdz1u.public.blob.vercel-storage.com"],
+      domains: ["https://image-proxy.simon-rosebrock.workers.dev", "unhqdt5ayffgdz1u.public.blob.vercel-storage.com"],
     },
   };
   
