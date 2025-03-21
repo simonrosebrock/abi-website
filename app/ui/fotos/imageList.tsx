9a74989487f1574c69269b3b20d9890a0ff6f190
MrGameSucht <117409993+MrGameSucht@users.noreply.github.com>
2025-03-21 23:35:39 +0100

    small image fix

--- app/ui/fotos/imageList.tsx
+++ app/ui/fotos/imageList.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image"
 import ImageModal from "@/app/ui/fotos/imageModal"
 import { GetServerSideProps } from 'next';
+import { getCleanUser } from "@/app/lib/miniFuncs";
 
 
 type imageListType = string[]
@@ -22,11 +23,16 @@ export default function ImageList({images, token}: {images: imageListType, token
             {
                 images.map((image: string) => (
                     <div key={`div/${image}`}>
-                        <button className="btn bg-white border-2 border-gray-200 w-[125px] h-[125px] xs:w-[150px] xs:h-[150px] sm:w-[200px] sm:h-[200px] lg:w-[250px] lg:h-[250px] rounded-lg flex items-center justify-center"
+                        <button className="relative btn bg-white border-2 border-gray-200 w-[125px] h-[125px] xs:w-[150px] xs:h-[150px] sm:w-[200px] sm:h-[200px] lg:w-[250px] lg:h-[250px] rounded-lg flex items-center justify-center"
                         onClick={() => {
                             const modal = document.getElementById(image) as HTMLDialogElement;
                             modal.showModal();
                         }}>
+                            <div data-tip={getCleanUser(image.split("/")[1])} className="tooltip tooltip-right absolute w-[50px] h-[25px] xs:w-[60px] xs:h-[30px] sm:w-[80px] sm:h-[40px] rounded-lg flex items-center justify-center bg-white border-2 border-gray-200 left-3 top-3">
+                                <span className="text-xs">{
+                                    getCleanUser(image.split("/")[1]).split(" ")[0]
+                                }</span>
+                            </div>
                             <img
                                 src={`https://image-proxy.simon-rosebrock.workers.dev/getImage?url=${image}&token=${token}&quality=low`}
                                 alt={``}
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ["https://image-proxy.simon-rosebrock.workers.dev", "unhqdt5ayffgdz1u.public.blob.vercel-storage.com"],
+      domains: ["image-proxy.simon-rosebrock.workers.dev", "unhqdt5ayffgdz1u.public.blob.vercel-storage.com"],
     },
   };
   
